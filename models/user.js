1d36c9001da83730fd333e96a58aa2ec4ed66f22
04vibs <04vibs@gmail.com>
2019-05-21 12:07:03 +0530

    user-routes-updated

--- models/user.js
+++ models/user.js
@@ -1,9 +1,25 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username:String,
-    email:String,
-    password:String
+    name:{
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    email:{
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024
+    }
 
 });
 
--- routes/users.js
+++ routes/users.js
@@ -7,19 +7,15 @@ router.get('/',(req,res)=>{
     res.send('Hello world');
 })
 
-router.post('/',(req,res)=>{
-    console.log('Inside post users');
-  let  user = new User(_.pick(req.body, ['name', 'email', 'password']));
-   console.log(_.pick(req.body, ['name', 'email', 'password']))
-    
-    fuser =  User.findOne({ email: req.body.email })
-    
-    if(!fuser){
-    return res.status(400).send('User already registered.');
-   }else{
-       user.save();
-       return res.send(user);
-   }
+router.post('/',async(req,res)=>{
+
+    let user = await User.findOne({ email: req.body.email });
+
+    if (user) return res.status(400).send('User already registered.');
+        user = new User(_.pick(req.body, ['name', 'email', 'password']));
+        await user.save();
+        return res.send(user);
+
 })
 
 module.exports = router;
