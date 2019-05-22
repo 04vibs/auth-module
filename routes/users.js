58cb81010383dda132ead2b1f714681669c9c4d5
04vibs <04vibs@gmail.com>
2019-05-22 20:54:35 +0530

    hashing

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const Joi = require('joi');
+const bcrypt = require('bcrypt');
 const router = express.Router();
 const {User} = require('../models/user');
 const _ = require('lodash');
 
 router.get('/',async(req,res)=>{
     
-    let user = await User.findOne({
-        email: req.body.email,
-        password: req.body.password
-    });
-    if(!user) return res.status(400).send('email Id or password does not match');
-        return res.status(200).send(user)
-})
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send('Invalid email or password.');
+  
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) return res.status(400).send('Invalid email or password.');
+    
+    return res.status(200).send(_.pick(user,['_id','name','email']));;
+    
+    })
 
 router.post('/',async(req,res)=>{
 
@@ -21,9 +24,14 @@ router.post('/',async(req,res)=>{
 
     let user = await User.findOne({ email: req.body.email });
     if (user) return res.status(400).send('User already registered.');
-        user = new User(_.pick(req.body, ['name', 'email', 'password']));
-        await user.save();
-        return res.send(user);
+       
+    user = new User(_.pick(req.body, ['name', 'email', 'password']));
+        
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password,salt);
+        
+    await user.save();
+    return res.send(_.pick(user,['_id','name','email']));
 
 });
 
