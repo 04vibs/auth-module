1c92c449c8308040ece853c09655987ba6642ba3
04vibs <04vibs@gmail.com>
2019-05-22 12:52:57 +0530

    validation-added

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Joi = require('joi');
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -22,7 +23,19 @@ const userSchema = new mongoose.Schema({
     }
 
 });
-
 const User = mongoose.model('User',userSchema);
 
+// function validateUser(user) {
+//     const schema = {
+//       //name: Joi.string().min(3).max(50).required(),
+//       email: Joi.string().min(5).max(255).required().email(),
+//       password: Joi.string().min(5).max(255).required()
+//     };
+  
+//     return Joi.validate(user, schema);
+//   }
+
+
+
 exports.User = User;
+// exports.validateUser = validateUser;
--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,39 @@
 const express = require('express');
+const Joi = require('joi');
 const router = express.Router();
 const {User} = require('../models/user');
 const _ = require('lodash');
 
-router.get('/',(req,res)=>{
-    res.send('Hello world');
+router.get('/',async(req,res)=>{
+    
+    let user = await User.findOne({
+        email: req.body.email,
+        password: req.body.password
+    });
+    if(!user) return res.status(400).send('email Id or password does not match');
+        return res.status(200).send(user)
 })
 
 router.post('/',async(req,res)=>{
 
-    let user = await User.findOne({ email: req.body.email });
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
+    let user = await User.findOne({ email: req.body.email });
     if (user) return res.status(400).send('User already registered.');
         user = new User(_.pick(req.body, ['name', 'email', 'password']));
         await user.save();
         return res.send(user);
 
-})
+});
 
+function validate(req) {
+    const schema = {
+        name: Joi.string().min(5).max(255).required(),
+      email: Joi.string().min(5).max(255).required().email(),
+      password: Joi.string().min(5).max(255).required()
+    };
+  
+    return Joi.validate(req, schema);
+}
 module.exports = router;
