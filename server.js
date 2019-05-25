111431df2631785fd5adaa3ddccf1cbab1596dd5
PRASHANT PURI <04vibs@gmail.com>
2019-05-25 14:29:26 +0530

    Add files via upload
    jwt-added

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
+const config = require('config');
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const users = require('./routes/users')
+const users = require('./routes/users');
+const auth = require('./routes/auth');
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use('/api/users',users);
+app.use('/api/auth', auth);
+
+// if(!config.get('jwtPrivateKey')){
+//     console.log(config)
+//     console.error('FATAL ERROR: jwtPrivateKey is not defined.');
+//     process.exit(1);
+// }
 
 mongoose.connect('mongodb://localhost/authentication')
     .then(()=>console.log('connected to mongob..'))
