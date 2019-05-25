d964e8ea4358b9843eb3ef966c096fa3eb89f1a2
04vibs <04vibs@gmail.com>
2019-05-25 16:30:05 +0530

    auth-updated

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,15 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const Joi = require('joi');
 const bcrypt = require('bcrypt');
 const router = express.Router();
 const {User} = require('../models/user');
 const _ = require('lodash');
 
-router.get('/',async(req,res)=>{
-    
-    let user = await User.findOne({ email: req.body.email });
-    if (!user) return res.status(400).send('Invalid email or password.');
-  
-    const validPassword = await bcrypt.compare(req.body.password, user.password);
-    if (!validPassword) return res.status(400).send('Invalid email or password.');
-    
-    return res.status(200).send(_.pick(user,['_id','name','email']));;
-    
-    })
+router.get('/me',  async (req, res) => {
+    const user = await User.findById(req.body._id).select('-password');
+    res.send(user);
+  });
 
 router.post('/',async(req,res)=>{
 
@@ -35,13 +29,4 @@ router.post('/',async(req,res)=>{
 
 });
 
-function validate(req) {
-    const schema = {
-        name: Joi.string().min(5).max(255).required(),
-      email: Joi.string().min(5).max(255).required().email(),
-      password: Joi.string().min(5).max(255).required()
-    };
-  
-    return Joi.validate(req, schema);
-}
 module.exports = router;
